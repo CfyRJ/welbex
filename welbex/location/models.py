56daaabd092c393d3558990c500c545c7e86c242
Alexandr Kositskiy <koso-96@mail.ru>
2024-03-26 21:31:45 +0300

    location app has been added

---
+++ welbex/location/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class locations(models.Model):
+    zip = models.IntegerField(unique=True)
+    lat = models.FloatField()
+    lng = models.FloatField()
+    city = models.CharField(max_length=50, verbose_name="City")
+    state_name = models.CharField(max_length=50, verbose_name="State")
+
+    def __str__(self) -> str:
+        return self.city
